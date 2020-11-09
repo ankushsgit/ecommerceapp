01397ecccb75587d28efb102f8f9fce43171fc1d
ankushsgit <aranapure@nisum.com>
2020-11-09 13:38:13 +0530

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <Entry />
-      <TheFirstNavBar/>
-      <TheSecondnavBar/>
-      <TheSideBar/>
-    <Products/>
   </div>
 </template>
 
 <script>
-import Entry from './components/Entry.vue';
-import Products from'./components/Products.vue';
-import TheFirstNavBar from "./components/UI/TheFirstNavBar";
-import TheSecondnavBar from "./components/UI/TheSecondNavBar.vue";
-import TheSideBar from "./components/UI/TheSideBar.vue";
-
-
 export default {
   name: 'app',
   components: {
-    Entry,
-    Products,
-    TheFirstNavBar,
-    TheSecondnavBar,
-    TheSideBar
 
   }
 }

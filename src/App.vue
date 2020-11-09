263dc79eda71dfcffdc2cd67d7ce15e067e07130
ankushsgit <aranapure@nisum.com>
2020-11-09 11:35:20 +0530

    files added

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,20 @@
 <template>
   <div id="app">
     <Entry />
+    <Products/>
   </div>
 </template>
 
 <script>
-import Entry from './components/Entry.vue'
+import Entry from './components/Entry.vue';
+import Products from'./components/Products.vue';
+
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    Entry
+    Entry,
+    Products
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/components/Entry.vue
+++ src/components/Entry.vue
@@ -196,3 +196,4 @@ export default {
 </script>
 
 <style scoped></style>
+
